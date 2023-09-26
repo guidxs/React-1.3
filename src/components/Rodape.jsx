00b567e14df23c8d43893b7d1d77b050ebbba56b
Guilherme <guilhermesobreiro9@gmail.com>
2023-09-26 11:38:06 -0300

    estilizar depois

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,12 @@
+import {} from 'react'
+import { FaAngellist } from 'react-icons/fa';
+
+function Ropade (){
+  return(
+    <section >
+    <h3>@2023-todos os direitos reservados</h3>
+    <FaAngellist/>
+    </section>
+  )
+}
+export default Ropade
